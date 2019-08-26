4988b113b6b577cbda4fafa6c766893ae7ed5c26
Evgeny Kharitonov <kharitonov@devfair0136.h2.fair>
2019-08-26 08:23:00 -0700

    dumping all languages

---
+++ egg/zoo/language_bottleneck/guess_number/generate.sh
@@ -0,0 +1,3 @@
+for path in 2019_08_07_02_55_40 2019_08_04_04_26_21 2019_08_02_12_01_53 2019_08_02_13_15_54; do
+  python dump_languages.py --input_path=/private/home/kharitonov/nest/guess_number/$path --output_path=./languages
+done
