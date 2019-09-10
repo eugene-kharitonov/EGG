cb51224fe21d047a44a64c29997f8e612121c610
Evgeny Kharitonov <kharitonov@fb.com>
2019-09-10 20:02:07 +0200

    wip

--- egg/zoo/lang_emerge/dataloader.py
+++ egg/zoo/lang_emerge/dataloader.py
@@ -1,8 +1,10 @@
+import itertools
+import json
 import torch
-import itertools, pdb, json, random
+
 
 def load_from_file(path):
-    with open(path, 'r') as f: 
+    with open(path, 'r') as f:
         loaded = json.loads(f.read())
     return loaded
 
@@ -14,7 +16,8 @@ class Dataset:
         self.n_examples = dataset['numInst'][mode]
         self.n_tasks = len(dataset['taskDefn'])
 
-        self.n_values = {attr: len(vals) for attr, vals in dataset['props'].items()}
+        self.n_values = {attr: len(vals)
+                         for attr, vals in dataset['props'].items()}
         self.attr_val_vocab = sum([len(v) for v in dataset['props'].values()])
 
         # input vocab for answerer
@@ -24,7 +27,7 @@ class Dataset:
         q_out_vocab = [chr(ii + 97) for ii in range(q_vocab_size)]
         a_out_vocab = [chr(ii + 65) for ii in range(a_vocab_size)]
 
-        a_in_vocab =  q_out_vocab + a_out_vocab
+        a_in_vocab = q_out_vocab + a_out_vocab
         q_in_vocab = a_out_vocab + q_out_vocab + task_vocab
 
         self.n_attrs = len(dataset['attributes'])
@@ -39,23 +42,27 @@ class Dataset:
         for attr in dataset['attributes']:
             attr_vals.extend(dataset['props'][attr])
 
-        self.attr_vocab = {value:ii for ii, value in enumerate(attr_vals)}
-        self.inv_attr_vocab = {index:attr for attr, index in self.attr_vocab.items()}
+        self.attr_vocab = {value: ii for ii, value in enumerate(attr_vals)}
+        self.inv_attr_vocab = {index: attr for attr,
+                               index in self.attr_vocab.items()}
 
         # get encoding for attribute pairs
         self.attr_pair = itertools.product(attr_vals, repeat=2)
-        self.attr_pair_vocab = {value:ii for ii, value in enumerate(self.attr_pair)}
-        self.inv_attr_pair_vocab = {index:value for value, index in self.attr_pair_vocab.items()}
+        self.attr_pair_vocab = {value: ii for ii,
+                                value in enumerate(self.attr_pair)}
+        self.inv_attr_pair_vocab = {
+            index: value for value, index in self.attr_pair_vocab.items()}
 
         # Separate data loading for test/train
         self.data = torch.LongTensor(self.n_examples, self.n_attrs)
         for ii, attr_set in enumerate(dataset['split'][mode]):
-            self.data[ii] = torch.LongTensor([self.attr_vocab[at] for at in attr_set])
+            self.data[ii] = torch.LongTensor(
+                [self.attr_vocab[at] for at in attr_set])
 
         # TODO: should it be until n_examples?
         self.range_inds = torch.range(0, self.n_examples - 1).long()
 
-    def __len__(self): 
+    def __len__(self):
         return self.n_examples
 
     def __getitem__(self, idx):
@@ -65,9 +72,8 @@ class Dataset:
 if __name__ == '__main__':
     dataset = Dataset('./data/toy64_split_0.8.json', mode='test')
     train_loader = torch.utils.data.DataLoader(dataset,
-                              batch_size=32,
-                              shuffle=True, num_workers=1)
-
+                                               batch_size=32,
+                                               shuffle=True, num_workers=1)
 
     for b in train_loader:
         print(b)
