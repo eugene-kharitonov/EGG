7771fa09e44124e940a781483047568b09003ec9
Evgeny Kharitonov <kharitonov@fb.com>
2019-12-20 17:00:33 +0100

    bug

--- egg/core/stoch_prop.py
+++ egg/core/stoch_prop.py
@@ -1,5 +1,5 @@
 import torch
-import torch.distributions
+
 import doctest
 import torch.nn as nn
 from collections import defaultdict
@@ -9,37 +9,55 @@ class ReinforcableMultinomial(torch.autograd.Function):
     @staticmethod
     def forward(ctx, logits):
         d = torch.distributions.Categorical(logits=logits)
+        logits = (logits - logits.logsumexp(dim=-1, keepdim=True))#.log_softmax(dim=-1)
+        #logits.log_softmax(dim=-1)
         sample = d.sample()
         losses_storage = torch.zeros(logits.size(0), device=logits.device)
         ctx.save_for_backward(logits, sample, losses_storage)
 
         one_hot_sample = torch.zeros_like(
             logits).scatter_(-1, sample.unsqueeze(1), 1.0)
+
+        #print('sampled-1 logprobs', d.log_prob(sample), d.logits)
         return one_hot_sample
 
     @staticmethod
     def backward(ctx, _):
         logits, sample, loss = ctx.saved_tensors
-        grad = torch.zeros_like(logits).scatter_(-1,
-                                                 sample.unsqueeze(1), logits)
-        return -loss.unsqueeze(1) * grad
+
+        #grad = (-1.0 / logits).unsqueeze(-1).expand(sample.size(0), logits.size(0))
+        #grad = torch.zeros_like(logits) 
+
+
+        # expected batch_size x n_logits
+        grad = torch.zeros(sample.size(0), logits.size(1), device=sample.device)
+        for i in range(sample.size(0)):
+            grad[i, sample[i]] = -1.0 / logits[i, sample[i]]
+
+        print(grad, loss)#.size(), sample.size(), logits.size())
+        #return loss.unsqueeze(1) * grad
+        return loss.unsqueeze(1) * grad
 
 
 r_multinomial = ReinforcableMultinomial.apply
 
-
+# TODO: make it a module
 class StochContext:
-    def __init__(self):
-        self.baseline_storage = defaultdict(float)
+    def __init__(self, baseline=True):
+        self.baseline_storage = None # defaultdict(float)
         self.baseline_n = defaultdict(float)
+        self.baseline = baseline
 
     def propagate_loss(self, **losses):
         for name, loss in losses.items():
             grad_fn = loss.grad_fn
             value = loss.detach()
 
-            baselined_value = value - self.baseline_storage[name]
-            self.baseline_n[name] += 1
+            if not self.baseline:
+                baselined_value = value
+            else:
+                baselined_value = value - self.baseline_storage[name]
+                self.baseline_n[name] += 1
 
             assert grad_fn
             nodes = [grad_fn]
@@ -55,7 +73,8 @@ class StochContext:
                 if node.next_functions:
                     nodes.extend(x for (x, _) in node.next_functions if x)
 
-            self.baseline_storage[name] += (value.mean() -
+            if self.baseline:
+                self.baseline_storage[name] += (value.mean() -
                                             self.baseline_storage[name]) / self.baseline_n[name]
 
         return torch.stack([l for l in losses.values()]).sum()
--- tests/stoch_prop_tests.py
+++ tests/stoch_prop_tests.py
@@ -2,35 +2,93 @@ from egg.core.stoch_prop import StochContext, r_multinomial
 
 import torch
 import torch.distributions
-import doctest
 import torch.nn as nn
-from collections import defaultdict
 
 
-#torch.random.manual_seed(71)
-stoch_context = StochContext()
+def test_multiarm_bandit():
+    torch.random.manual_seed(71)
+    stoch_context = StochContext()
 
-log_probs = torch.zeros(size=(1, 10), requires_grad=True)
-#torch.nn.init.constant_(log_probs, 0.)
+    log_probs = torch.zeros(size=(1, 10), requires_grad=True)
+    optimizer = torch.optim.Adam([log_probs], lr=1e-1)
 
-optimizer = torch.optim.Adam([log_probs], lr=1e-1)
-mean_payouts = torch.tensor([[0, 10, 0, 0, 0, 0, 0, 0, 0, 0]]).float()
-#mean_payouts /= mean_payouts.sum()
+    mean_payouts = torch.tensor([[10, 0, 0, 0, 0, 0, 0, 0, 0, 0]]).float()
 
-for _ in range(100):
-    optimizer.zero_grad()
-    #randomized_payouts = mean_payouts
-    randomized_payouts = torch.randn((8, 10)) * 0.0 +  mean_payouts
+    for _ in range(100):
+        optimizer.zero_grad()
+        randomized_payouts = torch.randn((8, 10)) * 0.5 + mean_payouts
 
-    one_hot_actions = r_multinomial(log_probs.log_softmax(dim=-1).expand_as(randomized_payouts))
-    loss = (one_hot_actions * randomized_payouts).sum(dim=-1)
-    #print(loss, one_hot_actions.argmax(dim=-1))
+        one_hot_actions = r_multinomial(
+            log_probs.log_softmax(dim=-1).expand_as(randomized_payouts))
+        loss = -(one_hot_actions * randomized_payouts).sum(dim=-1)
 
-    stoch_context.propagate_loss(loss=loss).backward()
-    optimizer.step()
-    
-print(log_probs.softmax(dim=-1))
-stoch_context.baseline_storage
+        stoch_context.propagate_loss(loss=loss).backward()
+        optimizer.step()
+
+    # should be (0, 0)
+    assert log_probs.argmax().eq(0).all()
 
 
-# TODO: hack backward on a variable!
+def test_two_losses():
+    torch.random.manual_seed(71)
+    stoch_context = StochContext()
+
+    log_probs_1 = torch.zeros(size=(1, 5), requires_grad=True)
+    log_probs_2 = torch.zeros(size=(1, 5), requires_grad=True)
+
+    mean_payouts = torch.tensor([[10, 0, 0, 0, 0]]).float()
+
+    loss_1 = (mean_payouts * r_multinomial(log_probs_1.log_softmax(dim=-1))).sum()
+    loss_2 = (mean_payouts * r_multinomial(log_probs_2.log_softmax(dim=-1))).sum()
+
+    stoch_context.propagate_loss(loss_1=loss_1).backward()
+    assert log_probs_2.grad is None
+    assert log_probs_1.grad is not None
+
+    old_grad_1 = log_probs_1.grad.clone()
+    stoch_context.propagate_loss(loss_2=loss_2).backward()
+    assert log_probs_2.grad is not None
+
+    assert (log_probs_1.grad == old_grad_1).all()
+
+
+def test_is_policy_gradient():
+    mean_payouts = torch.randn(5)
+    mean_payouts.zero_()
+    mean_payouts[4] = 1
+
+    torch.random.manual_seed(72)
+    log_probs = torch.randn(1, 5)
+    log_probs.requires_grad_(True)
+
+    stoch_context = StochContext(baseline=False)
+    sample = r_multinomial(log_probs)#.log_softmax(dim=-1))
+    #print(sample)
+    loss_1 = (mean_payouts * sample).sum()
+    #print('payout: ', loss_1)
+    stoch_context.propagate_loss(loss=loss_1).backward()
+
+    stoch_prop_grad = log_probs.grad
+    assert stoch_prop_grad is not None
+
+    torch.random.manual_seed(72)
+    log_probs = torch.randn(1, 5)
+    log_probs.requires_grad_(True)
+
+    distr = torch.distributions.Categorical(logits=log_probs)#.log_softmax(dim=-1))
+    sample = distr.sample()
+    #print(sample)
+    sampled_log_prob = distr.log_prob(sample)
+
+    payout = mean_payouts[sample]
+    #print('payout: ', payout)
+    loss_2 = sampled_log_prob * mean_payouts[sample]
+    loss_2.backward()
+
+    policy_grad = log_probs.grad
+
+    assert torch.allclose(policy_grad, stoch_prop_grad), str(policy_grad) + ' ' + str(stoch_prop_grad)
+
+
+if __name__ == '__main__':
+    test_is_policy_gradient()
