0be533968eb9c10d5f5d3817489f344e94f77f18
Evgeny Kharitonov <kharitonov@devfair0136.h2.fair>
2020-08-25 03:46:40 -0700

    wip

--- egg/zoo/mnist_vae/train.py
+++ egg/zoo/mnist_vae/train.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import numpy as np
 import torch
 import torch.utils.data
 from torchvision import datasets, transforms, utils
@@ -11,11 +12,12 @@ from torch.nn import functional as F
 import egg.core as core
 import pathlib
 
+from .data import get_dsprites_dataloader
 
 class Sender(nn.Module):
-    def __init__(self, message_dim):
+    def __init__(self, message_dim, image_size=28*28):
         super(Sender, self).__init__()
-        self.fc1 = nn.Linear(784, 400)
+        self.fc1 = nn.Linear(image_size, 400)
         self.fc21 = nn.Linear(400, message_dim)
         self.fc22 = nn.Linear(400, message_dim)
 
@@ -27,10 +29,10 @@ class Sender(nn.Module):
 
 
 class Receiver(nn.Module):
-    def __init__(self, message_dim):
+    def __init__(self, message_dim, image_size=28*28):
         super(Receiver, self).__init__()
         self.fc3 = nn.Linear(message_dim, 400)
-        self.fc4 = nn.Linear(400, 784)
+        self.fc4 = nn.Linear(400, image_size)
 
     def forward(self, x):
         x = F.relu(self.fc3(x))
@@ -38,11 +40,12 @@ class Receiver(nn.Module):
 
 
 class VAE_Game(nn.Module):
-    def __init__(self, sender, receiver):
+    def __init__(self, sender, receiver, image_size):
         super().__init__()
 
         self.sender = sender
         self.receiver = receiver
+        self.image_size = image_size
 
     @staticmethod
     def reparameterize(mu, logvar):
@@ -53,7 +56,7 @@ class VAE_Game(nn.Module):
 
     def forward(self, *batch):
         sender_input = batch[0]
-        sender_input = sender_input.view(-1, 784)
+        sender_input = sender_input.view(-1, self.image_size)
         mu, logvar = self.sender(sender_input)
 
         if self.train:
@@ -83,9 +86,10 @@ class VAE_Game(nn.Module):
 
 
 class ImageDumpCallback(core.Callback):
-    def __init__(self, eval_dataset):
+    def __init__(self, eval_dataset, image_shape=(28, 28)):
         super().__init__()
         self.eval_dataset = eval_dataset
+        self.image_shape = image_shape
 
     def on_epoch_end(self, loss, logs, epoch):
         dump_dir = pathlib.Path.cwd() / 'dump' / str(epoch)
@@ -94,15 +98,19 @@ class ImageDumpCallback(core.Callback):
         state = self.trainer.game.train
         self.trainer.game.eval()
 
+        l = len(self.eval_dataset)
+
         for i in range(5):
-            example = self.eval_dataset[i]
+            example_id = np.random.randint(0, l)
+            example = self.eval_dataset[example_id]
+
             example = core.move_to(example, torch.device('cuda'))
             _, interaction = self.trainer.game(*example)
 
             image = example[0][0]
 
-            output = interaction.receiver_output.view(28, 28)
-            image = image.view(28, 28)
+            output = interaction.receiver_output.view(*self.image_shape)
+            image = image.view(*self.image_shape)
             utils.save_image(
                 torch.cat([image, output], dim=1), dump_dir / (str(i) + '.png'))
         self.trainer.game.train(state)
@@ -113,22 +121,35 @@ def main(params):
     kwargs = {'num_workers': 1, 'pin_memory': True} if opts.cuda else {}
     transform = transforms.ToTensor()
 
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data', train=True, download=True,
-                       transform=transform),
-        batch_size=opts.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data', train=False, transform=transform),
-        batch_size=opts.batch_size, shuffle=True, **kwargs)
+    data = 'dsprite'
+    assert data in ['mnist', 'dsprite']
 
-    sender = Sender(opts.vocab_size)
-    receiver = Receiver(opts.vocab_size)
-    game = VAE_Game(sender, receiver)
+    if data == 'mnist':
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('./data', train=True, download=True,
+                           transform=transform),
+            batch_size=opts.batch_size, shuffle=True, **kwargs)
+        test_loader = torch.utils.data.DataLoader(
+            datasets.MNIST('./data', train=False, transform=transform),
+            batch_size=opts.batch_size, shuffle=True, **kwargs)
+
+        image_shape = (28, 28)
+    elif data == 'dsprite':
+        train_loader, test_loader = get_dsprites_dataloader(path_to_data='egg/zoo/mnist_vae/dsprites.npz',
+                                                            batch_size=opts.batch_size)
+        image_shape = (64, 64)
+
+    image_size = np.prod(image_shape)
+
+    sender = Sender(opts.vocab_size, image_size=image_size)
+    receiver = Receiver(opts.vocab_size, image_size=image_size)
+    game = VAE_Game(sender, receiver, image_size=image_size)
     optimizer = core.build_optimizer(game.parameters())
 
     # initialize and launch the trainer
     trainer = core.Trainer(game=game, optimizer=optimizer, train_data=train_loader, validation_data=test_loader,
-                           callbacks=[core.ConsoleLogger(as_json=True, print_train_loss=True), ImageDumpCallback(test_loader.dataset)])
+                           callbacks=[core.ConsoleLogger(as_json=True, print_train_loss=True), 
+                           ImageDumpCallback(test_loader.dataset, image_shape=image_shape)])
     trainer.train(n_epochs=opts.n_epochs)
 
     core.close()
